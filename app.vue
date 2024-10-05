527cc97c6bf2cdfb35e4895bd7437d37a7b22300
Mickael Vidal <mickaelvidal51@gmail.com>
2024-10-05 14:56:38 +0200

    First commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="tc-modal">
+    <h2>New card</h2>
+    <input class="tc-input-primary" placeholder="Front side*" type="text">
+    <input class="tc-input-primary" placeholder="Back side*" type="text">
+    <textarea class="tc-textarea-primary" placeholder="Note"></textarea>
+    <input type="submit" class="tc-button-primary" value="Add">
+  </div>
+</template>
