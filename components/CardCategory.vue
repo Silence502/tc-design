94a5b95cdda09b7224896866831a4faae1bf9bc3
Mickael VIDAL <mvidal@agendize.com>
2024-10-10 13:31:23 +0200

    Card category color

--- components/CardCategory.vue
+++ components/CardCategory.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts">
-import {X} from "lucide-vue-next";
+import {Palette} from "lucide-vue-next";
 </script>
 
 <template>
   <div class="tc-card-category">
     <div class="tc-card-category_header">
-      <button class="tc-cross">
-        <X/>
+      <button class="tc-palette">
+        <Palette/>
       </button>
     </div>
     <div class="tc-card-category_content">
       <h6>
         <slot></slot>
       </h6>
+    </div>
+    <div class="tc-card-category_color">
+
     </div>
   </div>
 </template>
